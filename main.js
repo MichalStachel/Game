2e0c86a92a0e7d770326142996ba51b5337232a6
Michał Stachel <michal.stachel@gmail.com>
2019-07-08 14:23:59 +0200

    add teleport/hidebtn after start improve collistion

--- main.js
+++ main.js
@@ -1,74 +1,135 @@
 const car = document.querySelector('img');
-let speed = 10;
+const timeText = document.querySelector('p');
+const teleportText = document.getElementById('teleportText');
+const btn = document.querySelector('button');
+let rotate = 0;
+let speed = 0;
 let carX = 200;
 let carY = 200;
-let positionTop = car.offsetTop;
-let positionLeft = car.offsetLeft;
+let time = 60;
+teleportCountDown = 5;
 let flag = false;
-let height = window.innerHeight;
-let width = window.innerWidth;
+let gameActive = false;
 car.style.left = `${carX}px`;
 car.style.top = `${carY}px`;
 
-car.addEventListener('mousedown', () => {
+car.addEventListener('click', () => {
   flag = !flag;
+  alert('Mouse Win!!!')
+  time = -1;
 })
+
+// moving car by mouse
 car.addEventListener('mousemove', (e) => {
   if (flag == true) {
     carX = e.clientX;
     carY = e.clientY;
-    car.style.left = `${carX-30}px`;
-    car.style.top = `${carY-50}px`;
+    car.style.left = `${carX-10}px`;
+    car.style.top = `${carY-30}px`;
     car.style.filter = 'brightness(0.5)';
   } else {
     car.style.filter = 'brightness(1)';
   }
 });
 
-
-if (positionTop === height) {
-  clearInterval(interval);
-  speed = 15;
+// Timer
+if (gameActive === false) {
+  car.style.display = 'none';
 }
+btn.addEventListener('click', () => {
+  time = 60;
+  teleportCountDown = 5;
+  gameActive = true;
+  carX = 200;
+  carY = 200;
+  car.style.left = `${carX}px`;
+  car.style.top = `${carY}px`;
+  const timer = setInterval(() => {
+    timeText.innerText = time;
+    teleportText.innerText = `Teleports left:${teleportCountDown}`;
+    if (time < 0) {
+      clearInterval(timer);
+      gameActive = false;
+    } else if (time === 0) {
+      alert('Car Win');
+      gameActive = false;
+      btn.style.display = 'flex';
+    }
+    if (gameActive === true) {
+      btn.style.display = 'none';
+      car.style.display = 'block';
+    } else {
+      btn.style.display = 'flex';
+      car.style.display = 'none';
+    }
+    time -= 1;
+    // console.log(gameActive);
+  }, 1000);
+})
 
 
+
+// move car
 document.addEventListener('keydown', (e) => {
   const interval = setInterval(() => {
-    speed += 1;
+    speed += 4;
     positionTop = car.offsetTop;
     positionLeft = car.offsetLeft;
-    console.log(positionTop)
-    console.log(positionLeft)
-    // console.log(speed)
-  }, 800);
+  }, 100);
   document.addEventListener('keyup', () => {
     clearInterval(interval);
-    speed = 15;
+    speed = 45;
   });
 
+  console.log(window.innerHeight, window.innerWidth)
+  if (carX <= 0 || carY <= 0 || carY >= window.innerHeight - 50 || carX >= innerWidth - 100) {
+    clearInterval(interval);
+    speed = 0;
+    alert('Mouse win')
+  }
 
+
+  // rotate car
   if (e.keyCode === 40) {
     carY += speed;
+    rotate = 180;
     car.style.top = `${carY}px`;
-    car.style.transform = 'rotateZ(180deg)';
-    car.style.transition = '0.2s';
+    car.style.transform = `rotateZ(${rotate}deg)`;
   } else if (e.keyCode === 38) {
     carY -= speed;
+    rotate = 0;
     car.style.top = `${carY}px`;
-    car.style.transform = 'rotateZ(0deg)';
+    car.style.transform = `rotateZ(${rotate}deg)`;
   } else if (e.keyCode === 39) {
     carX += speed;
+    rotate = 90;
     car.style.left = `${carX}px`;
-    car.style.transform = 'rotateZ(90deg)';
+    car.style.transform = `rotateZ(${rotate}deg)`;
   } else if (e.keyCode === 37) {
     carX -= speed;
+    rotate = 270;
     car.style.left = `${carX}px`;
-    car.style.transform = 'rotateZ(-90deg)';
+    car.style.transform = `rotateZ(${rotate}deg)`;
   }
-
-
+  // Teleport
+  if (teleportCountDown <= 5 && teleportCountDown >= 1) {
+    if (e.keyCode === 32) {
+      let randomX = Math.floor(Math.random() * innerWidth / 1.4);
+      let randomY = Math.floor(Math.random() * innerHeight / 1.4);
+      console.log(randomX, randomY);
+      carX = randomX;
+      carY = randomY;
+      car.style.left = `${carX}px`;
+      car.style.top = `${carY}px`;
+      console.log(e.keyCode);
+      teleportCountDown -= 1;
+    }
+  }
+  // info
   if (e.keyCode === 72) {
-    alert('Game allows you to drive the car with arrows.You can also click the car and move it to choosen destination :D')
+    alert(`Game allows you to drive the car with arrows.You can also click the car and move it to choosen destination :D
+    Rules are simple catch the car with mouse.
+    By pressing space car can teleport to a random destination
+    Don't hit the wall or you will lose`)
   }
-
 })
